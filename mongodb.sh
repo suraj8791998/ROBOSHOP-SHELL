0ec1300a4af9d2b55cc96ae6aa8e958d94162a49
Suraj kumar <mulaveesalav@gmail.com>
2024-01-09 17:24:44 +0530

    files updating

--- mongodb.sh
+++ mongodb.sh
@@ -19,4 +19,33 @@ then
 
 fi
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE(){
+    
+    if [ $? -ne 0 ]
+    
+    then
+       echo -e "$2 $R FAILURE $N"
+       exit 1
+    
+    else
+       echo -e "$2 $G SUCCESS $N"
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOGFILE
+VALIDATE $? "COPYING MONGO REPO TO OUR YUM.REPO"
+
+yum install mongodb-org -y &>>LOGFILE
+VALIDATE $? "Installing Mongodb"
+
+systemctl enable mongod &>>LOGFILE
+VALIDATE $? "ENABLING MONGODB"
+
+systemctl start mongod &>>LOGFILE
+VALIDATE $? "STARTING MONGODB"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>LOGFILE
+VALIDATE $? "EDITED MONGODB CONFIGURATION"
+
+systemctl restart mongod &>>LOGFILE
+VALIDATE $? "RESTARTED MONGODB SERVICE"
