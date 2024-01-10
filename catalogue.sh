667112cb18ee18fcad86142c7f181b2c60b86c60
Suraj kumar <mulaveesalav@gmail.com>
2024-01-10 11:48:24 +0530

    files uploading

---
+++ catalogue.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "SETTING NODEJS VENDOR" 
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "INSTALLING NODEJS" 
+
+useradd roboshop &>>$LOGFILE
+
+VALIDATE $? "ADDING USER" 
+
+mkdir /app &>>$LOGFILE
+
+VALIDATE $? "CREATING A  FOLDER" 
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+
+VALIDATE $? "DOWNLOADING THE APPLICATION"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "MOVING INTO APPLICATION"
+
+unzip /tmp/catalogue.zip &>>$LOGFILE
+
+VALIDATE $? "UNZIPPING THE APPLICATION"
+
+npm install  &>>$LOGFILE
+
+VALIDATE $? "INSTALLING DEPENDENCIES"
+
+cp /home/centos/ROBOSHOP-SHELL/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+
+VALIDATE $? "COPYING THE SERVICE"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "RELOADING THE SERVICE"
+
+systemctl enable catalogue &>>$LOGFILE
+
+VALIDATE $? "ENABLING THE CATALOGUE"
+
+systemctl start catalogue &>>$LOGFILE
+
+VALIDATE $? "STARTING CATALOGUE"
+
+cp /home/centos/ROBOSHOP-SHELL/mongo.repo  /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+
+VALIDATE $? "COPYING MONGO-REPO"
+
+yum install mongodb-org-shell -y &>>$LOGFILE
+
+VALIDATE $? "INSTALLING MONGO-REPO CLIENT"
+
+mongo --host 172.31.91.132 </app/schema/catalogue.js &>>$LOGFILE
+
+VALIDATE $? "LOADING THE SCHEMEA"
