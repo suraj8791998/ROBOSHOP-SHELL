97eeb2c123d57c24854805fe254212f8b472040c
Suraj kumar <mulaveesalav@gmail.com>
2024-01-10 16:31:01 +0530

    Catalogue files

---
+++ rabbitmq.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$LOGFILE
+
+VALIDATE $? "Configure YUM Repos from the script provided by vendor"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$LOGFILE
+
+VALIDATE $? "Configure YUM Repos for RabbitMQ"
+
+yum install rabbitmq-server -y  &>>$LOGFILE
+
+VALIDATE $? "INSTALLING RABBITMQ"
+
+systemctl enable rabbitmq-server  &>>$LOGFILE
+
+VALIDATE $? "ENABLING RABBITMQ"
+
+systemctl start rabbitmq-server  &>>$LOGFILE
+
+VALIDATE $? "STARTING RABITMQ"
+
+rabbitmqctl add_user roboshop roboshop123 &>>$LOGFILE
+
+VALIDATE $? "ADDING USER AND PASSWORD"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGFILE
+
+VALIDATE $? "SETTING PERMISSIONS"
